a0ac8694ca40fa3b0cfd29fb60200dfb1da0585a
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2023-08-31 11:26:49 +1000

    added clap for config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+use clap::{crate_version, Command};
 use rev_buf_reader::RevBufReader;
-use std::env::args;
 use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::path::PathBuf;
 
 fn main() -> Result<(), io::Error> {
-	let args = args().collect::<Vec<String>>();
-	let default = &String::from(".");
-	let user_path = args.get(1).unwrap_or(default);
+	let matches = Command::new("checkfile")
+		.about("A command line tool that logs the checksum and last 50 lines of each file in a folder")
+		.author("Dominik Wilkowski <hi@dominik-wilkowski.com>")
+		.version(crate_version!())
+		.arg(
+			clap::arg!([dir] "Set the directory you want to check")
+				.required(false)
+				.value_parser(clap::value_parser!(PathBuf))
+				.default_value("."),
+		)
+		.arg(
+			clap::arg!(-l --lines <VALUE> "Set the lines of each")
+				.required(false)
+				.value_parser(clap::value_parser!(usize))
+				.default_value("50"),
+		)
+		.arg(
+			clap::arg!(-o --output <FILE> "Set the output file")
+				.required(false)
+				.value_parser(clap::value_parser!(PathBuf))
+				.default_value("./checkfile.log"),
+		)
+		.get_matches();
+
+	let user_path = matches.get_one::<PathBuf>("dir").unwrap();
+	let user_lines = matches.get_one::<usize>("lines").unwrap();
+	let user_output = matches.get_one::<PathBuf>("output").unwrap();
 	let mut contents = String::from("");
 
 	let files = fs::read_dir(user_path)?
@@ -28,12 +52,15 @@ fn main() -> Result<(), io::Error> {
 		contents.push_str(&format!("## NAME {}\n## HASH {hash}\n", this_path.display()));
 
 		let file = fs::File::open(this_path);
-		let lines = lines_from_file(&file.unwrap(), 50);
+		let lines = lines_from_file(&file.unwrap(), *user_lines);
 		contents.push_str(&format!("-->\n{}\n<--\n", lines));
 	}
 
-	fs::write("checkfile.log", contents)?;
-	println!("\x1b[2F\x1b[0K\n\x1b[2kLogs for {files_count} files written successfully");
+	fs::write(user_output, contents)?;
+	println!(
+		"\x1b[2F\x1b[0K\n\x1b[2kThe log for {files_count} files was written successfully to {}",
+		user_output.display()
+	);
 	Ok(())
 }
 
